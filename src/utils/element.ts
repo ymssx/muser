550b00446b2b6aa2b3b101173647a98efa6df8a5
yami <yangming.yami@bytedance.com>
2021-07-13 09:28:50 +0800

    feat: child proxy

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -1,5 +1,27 @@
-import type Element from "src/element"
+import Element, { PROP } from "src/element"
+import CanvasProxy from 'src/canvasExtends';
 
 export const bindElements = (father: Element, children: Element) => {
   children.father = father;
+};
+
+export const getChildProxy = (elementMap: { [name: string]: Element }, father: Element) => {
+  return new Proxy({}, {
+    get(_, name: string) {
+      const element = elementMap[name];
+
+      if (!father[PROP].elPainterMap[name]) {
+        bindElements(father, element);
+        const ext = new CanvasProxy(element);
+        // TODO
+        father[PROP].elPainterMap[name] = ext.paint.bind(ext);
+      }
+
+      if (father[PROP].isCollectingChilds) {
+        father[PROP].tempChildStack.push(element);
+      }
+
+      return elementMap[name];
+    }
+  });
 };
